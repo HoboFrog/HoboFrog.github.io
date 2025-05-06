5e03e496ed30cbd2141edf9260de738db258bb8d
HoboFrog <56703573+HoboFrog@users.noreply.github.com>
2025-05-06 16:40:49 -0500

    Update authController.js

--- blog-platform/server/controllers/authController.js
+++ blog-platform/server/controllers/authController.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.login = (req, res) => {
   const { username, password } = req.body;
-  if (username !== 'admin' || password !== 'password') {
+  if (username !== 'HoboFrog' || password !== 'HoboFrog101!') {
     return res.status(401).send('Invalid credentials');
   }
   const token = jwt.sign({ id: 'admin' }, process.env.JWT_SECRET);
